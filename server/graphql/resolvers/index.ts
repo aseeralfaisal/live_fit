60f66d249dfa7a2c5f28dad6968b2a4035cc0736
aseeralfaisal <aseeralfaisal@gmail.com>
2022-08-06 19:50:59 +0600

    updated getExercise()

--- server/graphql/resolvers/index.ts
+++ server/graphql/resolvers/index.ts
@@ -4,7 +4,7 @@ import { ApolloError } from 'apollo-server-express'
 const saltRounds = process.env.SALT_ROUNDS as unknown as number
 import axios from 'axios'
 
-interface argsType {
+interface userArgsType {
   user: string
   pass: string
 }
@@ -12,25 +12,9 @@ interface argsType {
 const resolvers = {
   Query: {
     getUser: () => User.findAll().then((item: any) => item),
-    getExercise: async () => {
-      try {
-        const BASE_URL = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/back`
-        const config = {
-          headers: {
-            'X-RapidAPI-Key':
-              'adc17d2ademsh0e623b3458ffb2dp19ade4jsn0d7711bcfd5f',
-            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-          },
-        }
-        const exercises = await axios.get(BASE_URL, config)
-        return exercises.data
-      } catch (err) {
-        console.log(err)
-      }
-    },
   },
   Mutation: {
-    async addUser(parent: string, args: argsType) {
+    async addUser(_: undefined, args: userArgsType) {
       const { user, pass } = args
       try {
         const salt = await bcrypt.genSalt(+saltRounds)
@@ -46,7 +30,7 @@ const resolvers = {
         // console.log(err)
       }
     },
-    async loginUser(parent: string, args: argsType) {
+    async loginUser(_: undefined, args: userArgsType) {
       const { user, pass } = args
       try {
         const findUser = await User.findOne({ where: { user: user } })
@@ -62,6 +46,22 @@ const resolvers = {
         }
       } catch (err) {
         // console.log(err)
+      }
+    },
+    async getExercise(_: undefined, { target }: { target: string }) {
+      try {
+        const BASE_URL = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${target}`
+        const config = {
+          headers: {
+            'X-RapidAPI-Key':
+              'adc17d2ademsh0e623b3458ffb2dp19ade4jsn0d7711bcfd5f',
+            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+          },
+        }
+        const exercises = await axios.get(BASE_URL, config)
+        return exercises.data
+      } catch (err) {
+        console.log(err)
       }
     },
   },
