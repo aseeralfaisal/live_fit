21c52985be4db28e66c26bc8b1461009e7adb66f
aseeralfaisal <aseeralfaisal@gmail.com>
2022-08-16 02:24:53 +0600

    workoutSlice.reducer added

--- client/redux/store.tsx
+++ client/redux/store.tsx
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { userSlice } from './states/userSlice'
+import { workoutSlice } from './states/workoutSlice'
 
 const reducer = combineReducers<any>({
   user: userSlice.reducer,
+  workout: workoutSlice.reducer
 })
 
 const store = configureStore({
