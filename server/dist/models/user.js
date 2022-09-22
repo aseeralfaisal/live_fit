1237d97a13352e9ff9447168c7b5509280af3d0e
aseeralfaisal <aseeralfaisal@gmail.com>
2022-09-22 21:28:25 +0600

    fix

--- server/dist/models/user.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const User = new mongoose.Schema({
-    user: String,
-    pass: String,
-}, { timestamps: true });
-export default mongoose.model('User', User);
-//# sourceMappingURL=user.js.map
--- server/dist/models/workouts.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import exercisesSchema from './exercise';
-const Workouts = new mongoose.Schema({
-    userName: String,
-    workoutName: String,
-    exercises: [exercisesSchema],
-}, { timestamps: true });
-export default mongoose.model('Workouts', Workouts);
-//# sourceMappingURL=workouts.js.map
